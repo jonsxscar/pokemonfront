4fcaa2f745605a5bbc6a6e6e3493854705cd2e92
jonsxscar <jonsxscar@gmail.com>
2023-09-13 02:04:06 -0500

    ojala

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
+import axios from "axios";
+
+//axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://pokemonbackreal-production.up.railway.app/';
+
 
 ReactDOM.render(
   <Provider store={store}>
